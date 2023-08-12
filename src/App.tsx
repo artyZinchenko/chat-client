f803d291fa52460a4420496c6451acd8f73a3357
artyZinchenko <zinchenko.artem96@gmail.com>
2023-08-12 16:19:06 +0400

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 import Chat from './Chat/Chat';
 
 function App() {
-    const socket = io('http://localhost:3001');
+    const socket = io('https://chat-server-qvs6.onrender.com');
     socket.connect();
     socket.on('error', (error) => {
         console.error('Socket error:', error);
